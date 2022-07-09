de9486bb1a5905d1c93303fff0b4791e05c17fa8
TinfoilOreo <96307303+TinfoilOreo@users.noreply.github.com>
2022-07-09 19:46:36 +0100

    Added functionality to get arg

--- main.c
+++ main.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(void) {
+    char arg[50];
+
+    int running = 1;
+    
+    while (running) {
+        fgets(arg, sizeof(arg), stdin);
+        running = strcmp(arg, "quit") != 1;
+
+        
+    }
+
+    return 0;
+}
