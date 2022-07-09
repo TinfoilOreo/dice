bc0913e2f761d8d8e4244a3971767fafde20bfd4
TinfoilOreo <96307303+TinfoilOreo@users.noreply.github.com>
2022-07-09 20:12:53 +0100

    unimaginative commit

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct dice {
+    int sides;
+    char name[50];
+} dice;
+
+typedef struct dice_registry {
+    dice dice[50];
+    int dice_count;
+} dice_registry;
+
+void push_dice(dice_registry registry, dice die) {
+    registry.dice[registry.dice_count] = die;
+    registry.dice_count++;
+}
+
 int main(void) {
-    char arg[50];
+    char arg[256];
+
+    dice_registry registry;
 
     int running = 1;
     
@@ -10,7 +27,28 @@ int main(void) {
         fgets(arg, sizeof(arg), stdin);
         running = strcmp(arg, "quit") != 1;
 
-        
+        char args[10][50];
+        int arg_idx = 0;
+        int ind_arg_idx = 0;
+        for (size_t i = 0; i < strlen(arg); i++) {
+            if (arg[i] == ' ') {
+                args[arg_idx][ind_arg_idx] = 0;
+                arg_idx++;
+                ind_arg_idx = 0;
+            }
+            else {
+                args[arg_idx][ind_arg_idx] = arg[i];
+                ind_arg_idx++;
+            }
+        }
+
+        if (strlen(arg) > 0) {
+            arg_idx++;
+        }
+
+        if (strcmp(args[0], "add") == 1) {
+            
+        }
     }
 
     return 0;
